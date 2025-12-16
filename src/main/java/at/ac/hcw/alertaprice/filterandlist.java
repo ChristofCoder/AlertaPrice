7fd683b4c222708b7cd403530c6ea53172f6a0eb
LiLiSa42 <lisa.lichtenschopf@gmx.at>
2025-12-16 19:37:43 +0100

    Lisa initial commit

---
+++ src/main/java/at/ac/hcw/alertaprice/filterandlist.java
@@ -0,0 +1,46 @@
+package at.ac.hcw.alertaprice;
+
+import java.util.*;
+
+import static at.ac.hcw.alertaprice.WebAlertManager.getWebAlerts;
+
+public class filterandlist {
+    Scanner scanner = new Scanner(System.in);
+    private ArrayList<WebAlert> filtersort = getWebAlerts();
+
+    public void filterAlert(){
+
+        System.out.println("Bitte Filtermethode wählen (1 = Alphabetisch, 2 = Preis");
+        int filtermethod = scanner.nextInt();
+
+
+        switch (filtermethod) {
+            case 1:
+                System.out.println("Bitte Reihenfolge wählen (1 = ASC, 2 = DESC)");
+                int sortorder = scanner.nextInt();
+                switch (sortorder) {
+                    case 1:
+                        filtersort.sort(Comparator.comparing(WebAlert::getName));
+                            System.out.println(filtersort);
+
+                    case 2:
+                        filtersort.sort(Comparator.comparing(WebAlert::getName).reversed());
+                        System.out.println(filtersort);
+
+                        /* Macht basically: for (WebAlert alert : filtersort) {
+                            filtersort.sort(Collections.reverseOrder());
+                            System.out.println(filtersort);*/
+
+                        break;
+                    default:
+                        throw new IllegalStateException("Unexpected value: " + sortorder);
+                }
+                break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + filtermethod);
+        }
+
+
+
+        }
+}
