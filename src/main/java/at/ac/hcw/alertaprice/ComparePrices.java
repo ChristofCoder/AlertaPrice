2962f9fefd6c2d2722bb9f3812f0d59c5b30fa70
Thomas Eder <tneder90@gmail.com>
2026-01-03 20:31:37 +0100

    quickfix ComparePrices

--- src/main/java/at/ac/hcw/alertaprice/ComparePrices.java
+++ src/main/java/at/ac/hcw/alertaprice/ComparePrices.java
@@ -3,8 +3,7 @@ package at.ac.hcw.alertaprice;
 public class ComparePrices {
 
     public void compareAlert(WebAlert alert){
-        if (alert.getPreviousValue() < alert.getCurrValue()){
-
+        if (Integer.parseInt(alert.getPreviousValue()) < Integer.parseInt(alert.getCurrValue())){
         }
     }
 }
