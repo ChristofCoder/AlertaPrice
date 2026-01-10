10c08f70ed1c1cfd092365e818e83a0fa8064a8d
LiLiSa42 <lisa.lichtenschopf@gmx.at>
2026-01-10 16:14:07 +0100

    Spielt sound bei Preisänderungen

---
+++ src/main/java/at/ac/hcw/alertaprice/PlaySound.java
@@ -0,0 +1,29 @@
+package at.ac.hcw.alertaprice;
+
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import java.io.File;
+
+// inspired by https://www.youtube.com/watch?v=-GhCdekgNNM
+
+public class PlaySound {
+    public static File sound;
+    Clip clip; //braucht man in playSound zum Abspielen
+
+
+    public void loadSound(){
+        sound = new File("Sound/Alert.wav");
+    }
+
+    public void playSound(File sound){
+        loadSound();
+
+        try {
+            clip = AudioSystem.getClip();
+            clip.open(AudioSystem.getAudioInputStream(sound));
+            clip.start();
+        } catch (Exception e) {
+            e.printStackTrace(); //gibt Liste der Methodenaufrufe, die zu Fehler führten aus
+        }
+    }
+}
